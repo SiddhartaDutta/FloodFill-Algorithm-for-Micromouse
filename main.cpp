573dae04c04988fad3d87758342f542263da3815
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-05-26 22:37:50 -0700

    Array fill works

--- main.cpp
+++ main.cpp
@@ -46,15 +46,29 @@ void fillMaze(){
 
     // for 16x16, 7 & 8
     int fillVal = mazeDimension - 2;
-    int fillValLimit = fillVal/2;
+    int fillValCopy = fillVal;
+
+    cout << fillVal/2 + 1 << endl;
 
     for(int x = 0; x < mazeDimension; x++){
-        //if(x < )
+        if(x < fillVal/2 + 1 && x != 0){
+            fillValCopy--;
+        } else if(x > (fillVal/2 + 1)){
+            fillValCopy++;
+        } 
+        
         for(int y = 0; y < mazeDimension; y++){
-            maze[x][y] = 1;
+            if(y < fillVal/2){
+                maze[x][y] = fillValCopy;
+                fillValCopy--;
+            } else if(y == fillVal/2 || y == (fillVal/2 + 1)){
+                maze[x][y] = fillValCopy;
+            } else {
+                fillValCopy++;
+                maze[x][y] = fillValCopy;
+            }
         }
     }
-
     
 }
 
